9c39fb8ae30f37768a8cf16c45f560c62963b5d1
dandean <danwdean@gmail.com>
2011-04-28 00:04:56 -0700

    Add support for partials.

--- test/express.test.js
+++ test/express.test.js
@@ -26,12 +26,20 @@ module.exports = {
       res.render("test2", { value: "abc" });
     });
 
+    app.get('/test3', function(req, res){
+      res.render("test3", { thing: "•" });
+    });
+
     assert.response(app, { url: '/test1' },
       { status: 200, body: "abc" }
     );
 
     assert.response(app, { url: '/test2' },
       { status: 200, body: "ABC" }
+    );
+
+    assert.response(app, { url: '/test3' },
+      { status: 200, body: '•\nhello This is partial 1 • !\nhello This is partial 2 •!\nhello This is partial 3 •!\nhello This is partial 4 •••!' }
     );
   }
 };
---
+++ test/partial4/index.mustache.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: "partial 4",
+  thing: "•••"
+};
